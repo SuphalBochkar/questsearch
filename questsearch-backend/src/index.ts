306e213092d9671d9958e9f9100dd3bed8ddffb4
Suphal Bochkar <suphalbochkar@gmail.com>
2025-01-25 16:31:37 +0530

    Fix: Build

--- questsearch-backend/src/index.ts
+++ questsearch-backend/src/index.ts
@@ -2,6 +2,7 @@ import path from "path";
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
 import { ProtoGrpcType } from "./questions";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -14,7 +15,7 @@ import {
 import { DBConnection } from "./db/database";
 
 const PORT = 8080;
-const PROTO_FILE = "../../proto/questions.proto";
+const PROTO_FILE = "../proto/questions.proto";
 
 const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE), {
   keepCase: true,
@@ -29,6 +30,10 @@ const grpcObj = grpc.loadPackageDefinition(
 ) as unknown as ProtoGrpcType;
 const questionPackage = grpcObj.questionPackage;
 
+export default (req: VercelRequest, res: VercelResponse) => {
+  res.status(200).send("Hello from Vercel!");
+};
+
 function main() {
   DBConnection();
   const server = new grpc.Server();
@@ -39,7 +44,7 @@ function main() {
   });
 
   server.bindAsync(
-    `127.0.0.1:${process.env.PORT || PORT}`,
+    `0.0.0.0:${process.env.PORT || PORT}`,
     grpc.ServerCredentials.createInsecure(),
     (_err, port) => {
       console.log(`Server is running on port ${port}`);
